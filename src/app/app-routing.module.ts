bd434b1633f4607d3e3f45c044d42337a236b38c
Daniel-Plescia <plesciadc@me.com>
2019-04-26 13:41:00 -0400

    changing back ryans super stupid changes!!!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'settings', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
